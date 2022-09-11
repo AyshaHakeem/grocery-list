bf52b96849e5510a7205a341644c7cac6508c438
Aysha Hakeem <74527579+AyshaHakeem@users.noreply.github.com>
2022-09-12 00:10:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,28 +10,76 @@ const list = document.querySelector('.grocery-list');
 let editElement;
 let editFlag = false;
 let editID = '';
+let index = localStorage.length;
+let itemId = '';
+let currentItem = ''
+let currentList = [];
+let id = ''
 //events
 //event for adding item
 form.addEventListener('submit', addItem);
-
 const clear = document.querySelector('.clear-btn');
+
+window.addEventListener('load', () => {
+    
+    Object.keys(localStorage).forEach((x)=>{
+        if(localStorage.getItem(x)){
+            //creating unique id
+            id = new Date().getTime().toString(); //getTime -> to milliseconds
+            if (!editFlag) {
+            const element = document.createElement('article');
+            let attr = document.createAttribute('data-id');
+            attr.value = id;
+            element.setAttributeNode(attr);
+            element.classList.add('grocery-item');
+            element.innerHTML = (`<p class='title'> ${localStorage.getItem(x)} </p>
+            <div class="btn-group">
+                <button type="button" class="edit-btn">
+                <button type="button" class="trash-btn">
+                <i class="fas fa-trash-alt"></i> </button>
+            </div> `);
+            //accessing delete & edit btn for later use
+            const toTrash = element.querySelector('.trash-btn');
+            toTrash.addEventListener('click', deleteItems);
+    
+            const toEdit = element.querySelector('.edit-btn');
+            toEdit.addEventListener('click', editItems);
+            //toEdit.addEventListener('click', editItems);
+            // embedding / appending to other element
+            list.appendChild(element);
+            container.classList.add('show');
+            //default text field line 67
+            setBackToDefault();
+    
+        } else if (value && editFlag) {
+            editElement.innerHTML = value;
+    
+            setBackToDefault();
+        } 
+        }
+    })
+
+})
+
+
 
 function addItem(e) {
     e.preventDefault(); //to prevent the form from immediate submission
     const value = grocery.value;
     //creating unique id
     const id = new Date().getTime().toString(); //getTime -> to milliseconds
-
     if (value && !editFlag) {
+        itemId = `item${id}`  // key to save item to localStorage
+        localStorage.setItem( itemId , value)
+
         const element = document.createElement('article');
         let attr = document.createAttribute('data-id');
         attr.value = id;
         element.setAttributeNode(attr);
         element.classList.add('grocery-item');
-        element.innerHTML = (`<p class='title'> ${value} </p>
+        element.innerHTML = (`<p class='title'> ${localStorage.getItem(itemId)} </p>
         <div class="btn-group">
             <button type="button" class="edit-btn">
-            <i class="fas fa-edit"></i> </button>
             <button type="button" class="trash-btn">
             <i class="fas fa-trash-alt"></i> </button>
         </div> `);
@@ -68,6 +116,7 @@ clear.addEventListener('click', clearItems);
 
 function clearItems(e) {
     e.preventDefault();
+    localStorage.clear();
     const items = document.querySelectorAll('.grocery-item');
     if (items.length > 0) {
         items.forEach(function(item) {
@@ -82,6 +131,9 @@ function clearItems(e) {
 function deleteItems(e) {
     e.preventDefault();
     const element = e.currentTarget.parentElement.parentElement;
+    // remove from local storage
+    let toDelete = Object.values(localStorage).indexOf(`${element.firstChild.innerText}`)
+    localStorage.removeItem(Object.keys(localStorage)[toDelete])
     list.removeChild(element);
     alerts('Item Removed', 'empty');
     if (list.children.length === 0) {
@@ -96,12 +148,14 @@ function editItems(e) {
     submitBtn.innerHTML = 'edit';
     const element = e.currentTarget.parentElement.parentElement;
     editElement = element.children[0];
-    // submitBtn.addEventListener('click', editFinal);
+    let toEdit= Object.keys(localStorage)[Object.values(localStorage).indexOf(`${editElement.innerText}`)]
+    // remove from local storage
+    console.log(editElement.innerText, toEdit, Object.keys(localStorage)[toEdit])
+    // update text
     grocery.value = editElement.innerHTML;
     editFlag = true;
     editID = element.dataset.id;
-
-
+    // localStorage.setItem(toEdit , editElement.innerText)
 }
 
 // alerts function
